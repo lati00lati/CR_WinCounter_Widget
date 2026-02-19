204ce4a933cd3d0e2570eadef1b178740411ab4c
lati00lati <88554676+lati00lati@users.noreply.github.com>
2026-02-18 18:41:09 -0600

    v1.0.0

---
+++ README.md
@@ -0,0 +1,53 @@
+# Clash Royale Win Counter Widget v1.0.0
+Automatically updates a win/loss counter for your stream, with very simple setup.
+
+## Downloading Widget
+Downloading via link is far simpler. However, if you'd like to add the widget in an existing widget, you can manually copy the sections over.
+
+**Via Link**
+1. Click the following link and sign-in ➜ https://seapi.c4ldas.com.br/overlays/install/1771475290023
+
+**Manually**
+1. Head to your StreamElements Overlay's ➜ https://streamelements.com/dashboard/overlays
+2. Create a new Overlay (New Overlay ➜ Start)
+3. Add a Custom Widget (Click the blue '+' icon in the bottom left corner ➜ Static/Custom ➜ Custom Widget)
+4. Copy each file and paste it in the corresponding tab (ie. HTML in the HTML tab)
+5. [Optional] Delete everything within the top and bottom brackets { } in the 'fields' section of the widget.
+
+## Setup
+1. Open the widget editor (If it's not already open)
+2. Click into **Layers ➜ Custom widget 1 ➜ Settings ➜ Settings**.
+3. Locate the 'Accounts Tags' text box. Put your Clash Royale Tag in that box.
+4. Click Save (Top right)  
+
+View [Multiple Accounts](#multiple-accounts) if you need to track more than 1 account.
+
+## Adding Widget to OBS
+1. In OBS, create a new 'Browser Source'
+2. In the StreamElements Overlay menu (NOT the Widget Editor), click the 3 dots in the bottom left corner of the widget, and press 'Copy URL'
+3. Paste the copied URL into the Browser Source URL on OBS.
+4. Set the Browser Source width to 1920 and the height to 1080.
+5. [Optional] Crop the source by holding ALT whild adjusting the boundaries of the widget.
+
+## Multiple Accounts
+By default, the API limits you to one account subscription. To track more accounts at once, you need to obtain a Clash Royale API token.
+1. Open https://developer.clashroyale.com/#/
+2. Login or Register
+3. Click 'My Account' from the dropdown in the top right.
+4. Create a New Key
+5. Add the IP '212.192.28.75' to the token. Give it a name and description.
+6. Save the key.
+7. Copy the full token
+8. Open the widget's settings (View [Setup](#setup) for instructions)
+9. Paste the token in the 'Clash Royale API Key' box
+10. Add the rest of your account tags to the 'Accounts Tags' text box. Separate each with a comma. Example: `#48GJ5KE, #9kgjn5, #84TkmeE`
+
+## Warnings
+Feel free to connect to the API for small personal projects, or customize the widget to your liking.
+- Max 1 WebSocket connection per IP
+- Max 1 WebSocket subscription without a Clash Royale Token
+- Max 40 WebSocket subscriptions with a Clash Royale Token
+- Abuse of the API will result in a permanent IP ban
+
+## Contact
+For feature requests, assistance with setup, or problems, feel free to create an issue (preferred) or message 'lati00lati' on Discord.
---
+++ js.js
@@ -0,0 +1,165 @@
+let wins = 0;
+let losses = 0;
+let streak = 0;
+let fieldData;
+let socket;
+let disconnectedForcibly;
+
+window.addEventListener('onEventReceived', function (obj) {
+  const listener = obj.detail.listener;
+  const event = obj.detail.event;
+
+  if (listener == "message") {
+    let isMod = event.data.badges.find(badge => badge.type == "moderator");
+
+    let [command, subcommand, ...args] = event.data.text.split(" ");
+    // {Potential for commands here in the future.
+  }
+
+  else if (event.listener == "widget-button") {
+    if (event.field == "add1Win") updateRecord(wins + 1, losses, streak + 1);
+    if (event.field == "add1Loss") updateRecord(wins, losses + 1, 0);
+    if (event.field == "remove1Win") updateRecord(Math.max(0, wins - 1), losses, streak);
+    if (event.field == "remove1Loss") updateRecord(wins, Math.max(0, losses - 1), streak);
+    if (event.field == "reset") updateRecord(0, 0, 0);
+  }
+});
+
+window.addEventListener('onWidgetLoad', async function (obj) {
+  fieldData = obj.detail.fieldData;
+  let username = obj.detail.channel.username;
+  await loadState();
+
+  if (!fieldData.accounts) return;
+
+  // Slight delay to avoid spamming API when changing variables
+  setTimeout(() => {
+    startSocket(username, fieldData.token);
+  }, 2000)
+
+  if (fieldData.resetWhenOffline) {
+    checkLastOnline(username);
+    setInterval(async () => {
+      checkLastOnline(username);
+    }, 301000) // 5m 1s - Cache for this endpoint is 5m
+  }
+
+  // Just in case we somehow get completely disconnected accidentally
+  setInterval(() => {
+    if (!disconnectedForcibly) startSocket(username, fieldData.token);
+  }, 10000)
+});
+
+/**
+ * Socket Connection
+ */
+function startSocket(username, token) {
+  if (socket?.active) return;
+
+  socket = io('https://api.lati00lati.org/clashroyale', {
+    transports: ['websocket'],
+    auth: {
+      username: username,
+      token: token,
+    }
+  });
+
+  socket.on('connect', () => {
+    let accounts = fieldData.accounts.split(",");
+    for (let i = 0; i < accounts.length; i++) {
+      socket.emit('subscribeToPlayer', { tag: accounts[i] });
+    };
+  });
+
+  socket.on('battle_result', (data) => {
+    console.log('Received battle_result')
+    let result = data.result;
+    let battle = data.battle;
+
+    if (result == 'win') updateRecord(wins + 1, losses, streak + 1)
+    else if (result == 'loss') updateRecord(wins, losses + 1, 0)
+    else if (result == 'tie') { }
+  });
+
+  socket.on('kick', (data) => {
+    let error = data.error;
+    let message = data.message;
+
+    disconnectedForcibly = true;
+    setText(error)
+    console.log(`Error: ${message}`)
+  })
+
+  socket.on("connect_error", (err) => {
+    console.log("Failed to connect: " + err)
+  });
+
+  socket.on('disconnect', (reason) => {
+    switch (reason) {
+      case 'io server disconnect':
+        console.log("Server disconnected.");
+        break
+
+      case 'io client disconnect':
+        console.log("Client disconnected.");
+        break
+
+      case 'reconnect failed':
+        console.log("Failed to reconnect.");
+        break
+
+      default:
+        console.log(reason);
+        break
+
+    }
+  });
+}
+
+async function checkLastOnline(username) {
+  if (wins == 0 && losses == 0) return;
+  
+  let response = await fetch(`https://decapi.me/twitch/uptime/${username}`);
+  let uptime = await response.text();
+
+  if (!uptime.includes("offline")) await SE_API.store.set("last_online", Date.now());
+  else {
+    let lastOnline = (await SE_API.store.get("last_online")) ?? 0;
+    if (Date.now() - lastOnline > 900000) { // 15m offline grace period for temporary disconnects
+      updateRecord(0, 0, 0);
+    }
+  }
+}
+
+async function updateRecord(newWins, newLosses, streak) {
+  wins = newWins;
+  losses = newLosses;
+
+  let totalWins = wins + fieldData.initialWins;
+  let totalLosses = losses + fieldData.initialLosses;
+
+  let newText = fieldData.displayText.replace("{wins}", totalWins).replace("{losses}", totalLosses).replace("{streak}", streak);
+
+  setText(newText);
+  saveState();
+}
+
+function setText(text) {
+  document.getElementById("winstreak_text").innerHTML = text;
+}
+
+function saveState() {
+  SE_API.store.set('lati_winstreak', {
+    wins: wins,
+    losses: losses,
+    streak: streak,
+  });
+}
+
+async function loadState() {
+  let savedData = await SE_API.store.get('lati_winstreak');
+  wins = savedData?.wins ?? 0;
+  losses = savedData?.losses ?? 0;
+  streak = savedData?.streak ?? 0;
+  await updateRecord(wins, losses, streak);
+}
